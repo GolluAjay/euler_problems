c2f7501caca6bc93045da2ffc0b54b32a6bff204
ajay130 <gollu@revidd.com>
2023-07-30 14:44:53 +0530

    jul 30 14:44

---
+++ pandigitalMultiples.js
@@ -0,0 +1,46 @@
+function findDuplicates(temp) {
+    let arr1 = temp.split("");
+    for (let x = 0; x < arr1.length; x++) {
+      for (let y = 0; y < arr1.length; y++) {
+        if (x != y && arr1[x] === arr1[y]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  function pandigitalMultiples(k) {
+    let max = 10 ** (k - 1) * (k + 1);
+    let largest = 0;
+    let i = 1;
+    while (largest < max) {
+      let s = "";
+      for (let j = 1; j <= k; j++) {
+        let arr = s.split("");
+        let flag = true;
+        if (!findDuplicates((i * j).toString())) {
+          break;
+        }
+        if (arr.length != 0) {
+          for (let x of (i*j).toString().split('')) {
+            if (arr.includes(x) || x === "0") {
+              flag = false;
+              break;
+            }
+          }
+          if (!flag) break;
+        }
+        s += i * j;
+      }
+      if (parseInt(s) > largest && s.split("").length === k) {
+          // console.log(i + " " + s);
+          largest = parseInt(s);
+      }
+      if(i>10**(Math.ceil(k/2))) break;
+      i++;
+    }
+    return largest;
+  }
+  
+  pandigitalMultiples(8);
+  
